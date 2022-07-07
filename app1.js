67fbbbd4aa1efa0ebea0f2d5a7ee225aec1fab42
alaardarweesh <darweesh_alaa@yahoo.com>
2022-07-07 08:31:23 +0300

    task8

---
+++ app1.js
@@ -0,0 +1,53 @@
+'use strict';
+const allUsers=[]
+
+function Employee(identity,name,department,level,image){
+    this.identity=identity;
+    this.name=name;
+    this.department=department;
+    this.level=level;
+    this.image=image;
+
+    this.salary=function(){
+
+    };
+allUsers.push(this);
+}
+
+
+console.log(allUsers);
+    const ghazi= new Employee("1000 ","Ghazi Samer ","Administration ","Senior ")
+    console.log(ghazi);
+    const lana= new Employee("1001 ","Lana Ali	","Finance ","Senior ")
+    const tamara= new Employee("1002 ","Tamara Ayoub ","Marketing ","Senior ")
+    const safi= new Employee("1003 ","Safi Walid ","Administration ","Mid-Senior ")
+    const omar= new Employee("1004 ","Omar Zaid	","Development ","Senior ")
+    const rana= new Employee("1005 ","Rana Saleh ","Development ","Junior ")
+    const hadi= new Employee("1006 ","Hadi Ahmad ","Finance ","Mid-Senior ")
+
+    Employee.prototype.salary=function(){
+        let s = Math.floor((Math.random() * y) + x);
+        console.log(s);
+        if (level=="junior "){
+            x==500;
+            y==1000;
+
+        }
+else if(level=="Mid-Senior "){
+    x==1000;
+    y==1500;
+
+}
+else if(level=="Senior "){
+    x==1500;
+    y==2000;
+    }
+
+const level=[];
+level.push(level);
+}
+    
+    
+
+
+    console.log(getRandom);

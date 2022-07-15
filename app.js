a7642db9b8aa0f275422ac549a44e6d98e85a653
alaardarweesh <darweesh_alaa@yahoo.com>
2022-07-15 23:17:48 +0300

    redoo

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ function Employee(identity,name,department,level,image){
    }
     
 
-console.log(allUsers)
 
 
-    const ghazi= new Employee("1000 ","Ghazi Samer ","Administration ","Senior ",)
-    const lana= new Employee("1001 ","Lana Ali	","Finance ","Senior ")
-    const tamara= new Employee("1002 ","Tamara Ayoub ","Marketing ","Senior ")
-    const safi= new Employee("1003 ","Safi Walid ","Administration ","Mid-Senior ")
-    const omar= new Employee("1004 ","Omar Zaid	","Development ","Senior ")
-    const rana= new Employee("1005 ","Rana Saleh ","Development ","JUNIOR     ")
-    const hadi= new Employee("1006 ","Hadi Ahmad ","Finance ","Mid-Senior ")
+
+    const ghazi= new Employee("1000 ","Ghazi Samer ","Administration ","Senior ","assits/Ghazi.jpg")
+    const lana= new Employee("1001 ","Lana Ali	","Finance ","Senior ","assits/Lana.jpg")
+    const tamara= new Employee("1002 ","Tamara Ayoub ","Marketing ","Senior ","assits/Tamara.jpg")
+    const safi= new Employee("1003 ","Safi Walid ","Administration ","Mid-Senior ","assits/Safi.jpg")
+    const omar= new Employee("1004 ","Omar Zaid	","Development ","Senior ","assits/Omar.jpg")
+    const rana= new Employee("1005 ","Rana Saleh ","Development ","JUNIOR     ","assits/Rana.jpg")
+    const hadi= new Employee("1006 ","Hadi Ahmad ","Finance ","Mid-Senior ", "assits/Ghazi.jpg")
 
       
    
@@ -36,7 +36,7 @@ console.log(allUsers)
         if (this.level.trim().toLowerCase()=="junior"){
            this.salary = randomIntFromInterval(500, 1000)
             
-    
+          
 
         }
         else if(this.level.trim().toLowerCase()=="mid-senior"){
@@ -51,23 +51,42 @@ console.log(allUsers)
          
       
     }
+
+
     
-    Employee.prototype.image=function(){
-        if(this.name.trim().toLowerCase="Ghazi Samer"){
-            
-            
-        }
-    }
   
-
+  
 Employee.prototype.addInfo=function(){
-    document.write("Name:   "+this.name+"      "+"-ID:"+this.identity+"-Department:"+this.department+"-level:"+this.level+" &nbsp; &nbsp;   -"+this.salary+"JOD&nbsp; &nbsp; <br><br>")
 
+    let divEl=document.getElementById("hai");
+    const lele=divEl.textContent ="Name:   "+this.name+"      "+"-ID:"+this.identity+"-Department:"+this.department+"-level:"+this.level+" &nbsp; &nbsp;   -"+this.salary+"JOD&nbsp; &nbsp; <br><br>"
+
+ const image= document.createElement("img")
+ image.setAttribute("src",this.image);
+ divEl.appendChild(image);
+
+ let divEl2=document.getElementById("hi");
+ const lele2=divEl.textContent ="Name:   "+this.name+"      "+"-ID:"+this.identity+"-Department:"+this.department+"-level:"+this.level+" &nbsp; &nbsp;   -"+this.salary+"JOD&nbsp; &nbsp; <br><br>"
+
+const image2= document.createElement("img2")
+image.setAttribute("src",this.image);
+divEl.appendChild(image);
+
+      
+    const pEl=document.createElement("p");
+    pEl.textContent="Name:   "+this.image+this.name+"      "+"-ID:"+this.identity+"-Department:"+this.department+"-level:"+this.level+" &nbsp; &nbsp;   -"+this.salary+"JOD"+ this.image
+    divEl.appendChild(pEl)
+  console.log(divEl)
+  
 } 
+console.log(this.image)
+
 
  for (var i=0; i<allUsers.length;i++){
     allUsers[i].salary();
     allUsers[i].addInfo();
+    
+
  }
 
 
@@ -75,3 +94,30 @@ function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
     
   }
+  //         imgGhazi.src="https://github.com/LTUC/new-prep-course-std/blob/main/Day10/Task/assets/Ghazi.jpg?raw=true"
+//         divEl.appendChild(imgGhazi);
+//     }
+    
+// else if(this.name.toLowerCase.trim()=="lana ali"){
+//     const imgLana=document.createElement("img")
+//     imgLana.src="https://github.com/LTUC/new-prep-course-std/blob/main/Day10/Task/assets/Lana.jpg?raw=true"
+//     divEl.appendChild(imgLana);
+// }
+    // Employee.prototype.addimg=function(){
+//     if(this.name.toLowerCase.trim()=="Ghazi Samer"){
+//         this.image=
+//     }
+// }
+    // const imgEl=document.createElement("img")
+    // imgEl.src="https://physicsworld.com/wp-content/uploads/2019/05/Sunshine.jpg";
+    // imgEl.alt="sun"
+    // const divEl=document.getElementsByTagName("div")
+    
+    // Employee.prototype.image=function(){
+    //     if(this.name.trim().toLowerCase="Ghazi Samer"){
+            
+            
+    //     }
+    // }
+        // divEl.appendChild(lele)
+//     if(this.name.toLowerCase.trim()=="ghazi samer"){
